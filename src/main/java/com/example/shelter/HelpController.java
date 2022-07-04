221862edfefafb1d273c413aa2777e1e34beb0c6
mmorfii <mmorfii.yes@gmail.com>
2022-07-04 14:47:51 +0300

    second commit

--- src/main/java/com/example/shelter/HelpController.java
+++ src/main/java/com/example/shelter/HelpController.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class HelpController {
@@ -19,6 +25,64 @@ public class HelpController {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
         return "help";
+    }
+
+    @GetMapping("/help/add")
+    public String HelpAdd(Model model) {
+        return "help-add";
+    }
+
+    @PostMapping("/help/add")
+    public String HelpPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/help";
+
+    }
+
+    @GetMapping("/help/{id}")
+    public String helpDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/help";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "help-details";
+    }
+
+    @GetMapping("/help/{id}/edit")
+    public String helpEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/help";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "help-edit";
+    }
+
+    @PostMapping("/help/{id}/edit")
+    public String HelpPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+
+        return "redirect:/help";
+
+    }
+
+    @PostMapping("/help/{id}/remove")
+    public String HelpPostDelete(@PathVariable(value = "id") long id, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+
+        return "redirect:/help";
+
     }
 
 
